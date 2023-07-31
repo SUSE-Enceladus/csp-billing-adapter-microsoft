56cb879415add05ad06abe92fb95d3f0055af7fc
KeithMnemonic <kberger@suse.com>
2023-07-31 10:00:14 -0400

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with open('requirements-dev.txt') as req_file:
 
 setup(
     name='csp-billing-adapter-microsoft',
-    version='0.1.0',
+    version='0.1.1',
     description='TBD',
     long_description=readme,
     long_description_content_type='text/markdown',
